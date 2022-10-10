b7999f3c58b7faa6139ebb1b3e916529a7483e4c
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-10 14:12:45 -0400

    added ball, ball style and velocity, added import to main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from unicodedata import numeric
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
 from kivy.vector import Vector
 
 class PongBall(Widget):
@@ -14,18 +15,26 @@ class PongBall(Widget):
     
     # referancelist property to use ball.velocity
     # this is shorthand
-    velocity = ReferanceListProperty(velocity_x, velocity_y)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
     
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
 class PongGame(Widget):
     
-    def update(self):
+    def update(self, dt):
         """
         Purpose: 
         call ball and movie it around
         """
+        # ball bounces of top and bottom
+        if (self.ball.y <0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+            
+        # ball bounces of left and right
+        if (self.ball.x <0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
+        
         pass
 
 class PongApp(App):
