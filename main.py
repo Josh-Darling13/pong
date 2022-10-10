29283c2882f3b1f82ffdbf9215d2b1bb7d79b683
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-10 13:50:15 -0400

    wrote python code to control the movement of the ball

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from unicodedata import numeric
 from kivy.app import App
 from kivy.uix.widget import Widget
+
+class PongBall(Widget):
+    #this make the "ball move"
+    # Kivy will design the ball
+    # a realtionship a kin to Javascript and CSS
+    
+    #velocity of the ball on x & y axis
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    
+    # referancelist property to use ball.velocity
+    # this is shorthand
+    velocity = ReferanceListProperty(velocity_x, velocity_y)
+    
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 class PongGame(Widget):
     pass
