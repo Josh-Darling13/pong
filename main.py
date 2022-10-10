887010ab0b36cab283c35e8c5dcc20fa3d20f2c0
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-10 13:38:17 -0400

    basic playing field created

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import kivy
-kivy.require('2.1.0') # replace with your current kivy version !
-
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 
+class PongGame(Widget):
+    pass
 
-class MyApp(App):
-
+class PongApp(App):
     def build(self):
-        return Label(text='Hello world')
-
-
+        return PongGame()
+    
 if __name__ == '__main__':
-    MyApp().run()
+    PongApp().run()
