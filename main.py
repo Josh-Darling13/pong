9e980653d0c774edfce504599e8d2d9aa9dda6ad
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-10 14:33:29 -0400

    animated ball

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
 from unicodedata import numeric
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import (
+    NumericProperty, ReferenceListProperty, ObjectProperty
+)
 from kivy.vector import Vector
 from kivy.clock import Clock
 from random import randint
 
 class PongBall(Widget):
-    #this make the "ball move"
-    # Kivy will design the ball
-    # a realtionship a kin to Javascript and CSS
+    """this make the "ball move" Kivy will design the ball a realtionship a kin to Javascript and CSS"""
     
     #velocity of the ball on x & y axis
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     
-    # referancelist property to use ball.velocity
-    # this is shorthand
+    """ referancelist property to use ball.velocity this is shorthand"""
     velocity = ReferenceListProperty(velocity_x, velocity_y)
     
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
 class PongGame(Widget):
+    ball = ObjectProperty(None)
+    
+    def serve_ball(self):
+        """puts the ball in motion"""
+        self.ball.center = self.center
+        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
     
     def update(self, dt):
         """
         Purpose: 
         call ball and movie it around
         """
-        # ball bounces of top and bottom
-        if (self.ball.y <0) or (self.ball.top > self.height):
+        self.ball.move()
+        
+        # bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
-            
-        # ball bounces of left and right
-        if (self.ball.x <0) or (self.ball.right > self.width):
+
+        # bounce off left and right
+        if (self.ball.x < 0) or (self.ball.right > self.width):
             self.ball.velocity_x *= -1
         
         pass
@@ -42,6 +49,7 @@ class PongGame(Widget):
 class PongApp(App):
     def build(self):
         game = PongGame()
+        game.serve_ball()
         Clock.schedule_interval(game.update, 1.0/60.0)
         return game
     
